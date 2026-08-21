a928a8dffcd615079841f168099828429831f4ba
pravatkumarmishra <pravatkumarmishrapkm@gmail.com>
2026-08-21 14:00:18 +0530

    Serve static resource form backend

---
+++ src/main/java/me/pravat/tinder_ai_backend/config/StaticResourceConfig.java
@@ -0,0 +1,14 @@
+package me.pravat.tinder_ai_backend.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class StaticResourceConfig implements WebMvcConfigurer {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("images/**")
+                .addResourceLocations("classpath:/static/images/");
+    }
+}
